ed12b1aa57b98c032c70c866b60984f67a1c198f
hgarra <arttu.rasanen@tuni.fi>
2023-09-25 23:31:42 +0300

    randomize remainder distribution, add error checks, format changes

--- app/main.f90
+++ app/main.f90
@@ -9,58 +9,109 @@
 program main
     implicit none
     
+    localize: block 
+    
     integer, parameter :: MAX_LEN = 20
     character (len=MAX_LEN) :: user_input
     integer :: num_people
     integer :: money
     integer, dimension(:), allocatable :: divisions
+    integer :: beginner
     
     write(unit=*, fmt='(a)', advance='no') 'Enter amount of money: '
     read *, user_input
+    
+    if (dec_to_int(user_input, money)) then
+        stop 'Error: Overflow or invalid input.'
+    end if
+    
+    if (money <= 0) then
+        stop 'Error: Invalid amount of money.'
+    end if
     
     write(unit=*, fmt='(a)', advance='no') 'Enter number of people: '
     read *, num_people
     
     if (num_people < 1) then
-        print '(a)', 'Error: invalid number of people.'
-        stop 1
+        stop 'Error: invalid number of people.'
     end if
+    
+    call line()
+    
+    call choose_beginner(beginner, num_people)
     
     allocate(divisions(1:num_people))
     
-    if (dec_to_int(user_input, money)) then
-        print '(a)', 'Error: Overflow or invalid input.'
-        stop 1
-    end if
-    
-    call divide_money(money, divisions)
+    call divide_money(money, divisions, beginner)
     call print_shares(divisions)
 
     deallocate(divisions)
+    
+    end block localize
 contains
 
-    ! Divides "total" amount of money into "shares" as fairly as 
+    ! Prints a line.
+    subroutine line()
+        print '(a)', repeat('-', 50)
+    end subroutine
+
+    ! Choose a random person who gets the remainder first.
+    subroutine choose_beginner(beginner, max_num)
+        integer, intent(out) :: beginner
+        integer, intent(in) :: max_num
+        real :: rn, rn_scaled
+        call random_number(rn)
+        rn_scaled = (rn * max_num) + 1.0
+        beginner = floor(rn_scaled)
+    end subroutine
+
+    ! Divide "total" amount of money into "shares" as fairly as 
     ! possible.
-    subroutine divide_money(total, shares)
+    subroutine divide_money(total, shares, starter)
         integer, intent(in) :: total
         integer, dimension(:), allocatable, intent(inout) :: shares
-        integer :: div
-        integer :: rem
-        integer :: i
-        integer :: j
+        integer, intent(in) :: starter
+        integer :: div, rem, i, j
         
         div = total / size(shares)
         rem = total - (div * size(shares))
         
-        do i = 1, size(shares)
-            shares(i) = div
-        end do
+!~         do i = 1, size(shares)
+!~             shares(i) = div
+!~         end do
+        shares  = div
         
-        j = 1
+        write(*, fmt=140) 'Individual share per person:', div/100.0
+        if (rem /= 0) then
+            write(*, fmt='(4x,a)', advance='no') 'Remainder:'
+            if (rem < 100) then
+                write(*, fmt='(x,a)', advance='no') '0'
+            end if
+            write(*, fmt='(f0.2)') rem/100.0
+            write(*, fmt='(4x,a)', advance='no') &
+                    'Distributing remainder to'
+            if (rem == 1) then
+                write(*, fmt='(x,a)', advance='no') 'person:'
+            else
+                write(*, fmt='(x,a)', advance='no') 'persons:'
+            end if
+        end if
+        
+        j = starter
         do i = rem, 1, -1
+            write(*, fmt='(x,a,i0)', advance='no') '#', j
             shares(j) = shares(j) + 1
             j = j + 1
+            if (j > size(shares)) then
+                j = 1
+            end if
         end do
+        
+        if (rem /= 0) then
+            write(*, '(a)') '.' // new_line('a')
+        end if
+        
+        140 format (4x,a,x,f0.2)
     end subroutine
     
     
@@ -70,18 +121,19 @@ contains
         integer :: sum_money
         integer :: i
         
-        print '(a)', repeat('-', 50)
-        
         sum_money = 0
         do i = 1, size(shares)
-            write(*, '(4x,a,i0,a,f0.2)') 'person #', i, ' receives: ', & 
+            write(*, fmt=150) 'person #', i, ' receives: ', & 
                 real(shares(i))/100.0
             
             sum_money = sum_money + shares(i)
         end do
         
-        print '(a)', repeat('-', 50)
-        print '(a,f0.2)', 'Total: ', real(sum_money)/100.0
+        call line()
+        write(*, fmt=160), 'Total: ', real(sum_money)/100.0
+        150 format (4x,a,i0,a,f0.2)
+        160 format (4x,a,f0.2)
+        
     end subroutine
     
     
